273ca812ffebdc485cc3207aae72cd24c0dab27a
Liucw <liucw.mail@gmail.com>
2016-02-23 17:02:00 +0800

    First commit & release v1.0.0

---
+++ README.md
@@ -0,0 +1,35 @@
+# generator-react-new-component [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url] [![Coverage percentage][coveralls-image]][coveralls-url]
+> Generator one react new component
+
+## Installation
+
+First, install [Yeoman](http://yeoman.io) and generator-react-new-component using [npm](https://www.npmjs.com/) (we assume you have pre-installed [node.js](https://nodejs.org/)).
+
+```bash
+npm install -g yo
+npm install -g generator-react-new-component
+```
+
+Then generate your new project:
+
+```bash
+yo react-new-component
+```
+
+## Getting To Know Yeoman
+
+Yeoman has a heart of gold. He&#39;s a person with feelings and opinions, but he&#39;s very easy to work with. If you think he&#39;s too opinionated, he can be easily convinced. Feel free to [learn more about him](http://yeoman.io/).
+
+## License
+
+MIT © [Liucw]()
+
+
+[npm-image]: https://badge.fury.io/js/generator-react-new-component.svg
+[npm-url]: https://npmjs.org/package/generator-react-new-component
+[travis-image]: https://travis-ci.org/booxood/generator-react-new-component.svg?branch=master
+[travis-url]: https://travis-ci.org/booxood/generator-react-new-component
+[daviddm-image]: https://david-dm.org/booxood/generator-react-new-component.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/booxood/generator-react-new-component
+[coveralls-image]: https://coveralls.io/repos/booxood/generator-react-new-component/badge.svg
+[coveralls-url]: https://coveralls.io/r/booxood/generator-react-new-component
---
+++ generators/app/index.js
@@ -0,0 +1,101 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+
+var inquirer = require('inquirer');
+var walkSync = require('walk-sync');
+
+module.exports = yeoman.generators.Base.extend({
+  prompting: function () {
+    var done = this.async();
+
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to the gnarly ' + chalk.red('generator-react-new-component') + ' generator!'
+    ));
+
+    var prompts = [{
+      type: 'input',
+      name: 'componentName',
+      message: 'What\'s name new component?',
+      validate: function() {
+        return true;
+      }
+    }, {
+      type: 'list',
+      name: 'componentPath',
+      message: 'Choices component path:',
+      choices: function() {
+        var dirs = [];
+        var reg = new RegExp('^client/(component|container)(s)?/([a-z]*/)*$');
+        var entries = walkSync.entries('.', {globs: ['client/**/*']})
+        entries.forEach(function(entry) {
+          if (entry.isDirectory() && reg.test(entry.relativePath)) {
+            dirs.push(entry.relativePath);
+          }
+        });
+        return dirs;
+      },
+      default: 'client/components/'
+    }, {
+      type: 'confirm',
+      name: 'isComponentDir',
+      message: 'Whether to use folder contains component files?',
+      default: true
+    }, {
+      type: 'list',
+      name: 'jsExtension',
+      message: 'Choices component file extension:',
+      choices: ['js', 'jsx'],
+      default: 'jsx'
+    }, {
+      type: 'list',
+      name: 'stylesheetExtension',
+      message: 'Choices no need component stylesheet file or extension:',
+      choices: [
+        { name: 'no need stylesheet file', value: false },
+        new inquirer.Separator(),
+        'css',
+        'less',
+        'scss'
+      ],
+      default: 'css'
+    }];
+
+    this.prompt(prompts, function (props) {
+      this.props = props;
+      done();
+    }.bind(this));
+  },
+
+  writing: function () {
+    var jsName = this.props.componentName + '.' + this.props.jsExtension;
+    var stylesheetName = this.props.componentName + '.' + this.props.stylesheetExtension;
+    var jsFullPath = this.props.componentPath;
+    var stylesheetFullPath = this.props.componentPath;
+
+    if (this.props.isComponentDir) {
+      jsFullPath += '/' + this.props.componentName + '/' + jsName;
+      stylesheetFullPath += '/' + this.props.componentName + '/' + stylesheetName;
+    } else {
+      jsFullPath += jsName;
+      stylesheetFullPath += stylesheetName;      
+    }
+
+    this.fs.copyTpl(
+      this.templatePath('componentFile'),
+      this.destinationPath(jsFullPath),
+      { componentName:  this.props.componentName,
+        needStylesheet: !!this.props.stylesheetExtension,
+        stylesheetFile:  stylesheetName }
+    );
+
+    if (!!this.props.stylesheetExtension) {
+      this.fs.copy(
+        this.templatePath('stylesheetFile'),
+        this.destinationPath(stylesheetFullPath)
+      );
+    }
+  }
+});
