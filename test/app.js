65f8f462592578f8377e3a66472339ff58a9276c
Liucw <liucw.mail@gmail.com>
2016-08-28 02:41:37 +0800

    Add test case

--- test/app.js
+++ test/app.js
@@ -7,13 +7,52 @@ describe('generator-react-new-component:app', function () {
   before(function (done) {
     helpers.run(path.join(__dirname, '../generators/app'))
       .withOptions({someOption: true})
-      .withPrompts({someAnswer: true})
+      .withPrompts({
+        componentName: 'Hello',
+        componentPath: 'src/',
+        isComponentDir: true,
+        jsExtension: 'jsx',
+        isAddProps: true,
+        stylesheetExtension: 'scss'
+      })
       .on('end', done);
   });
 
-  it('creates files', function () {
+  it('creates files and folder', function () {
     assert.file([
-      'dummyfile.txt'
+      'src/hello/hello.jsx',
+      'src/hello/hello.scss'
     ]);
+  });
+
+  it('Add PropTypes', function () {
+    assert.fileContent('src/hello/hello.jsx', /import React, { Component, PropTypes } from 'react'/);
+  });
+});
+
+describe('generator-react-new-component:app', function () {
+  before(function (done) {
+    helpers.run(path.join(__dirname, '../generators/app'))
+      .withOptions({someOption: true})
+      .withPrompts({
+        componentName: 'HelloWorld',
+        componentPath: 'src/',
+        isComponentDir: false,
+        jsExtension: 'js',
+        isAddProps: false,
+        stylesheetExtension: 'css'
+      })
+      .on('end', done);
+  });
+
+  it('creates files and no folder', function () {
+    assert.file([
+      'src/hello-world.js',
+      'src/hello-world.css'
+    ]);
+  });
+
+  it('No PropTypes', function () {
+    assert.fileContent('src/hello-world.js', /import React, { Component } from 'react'/);
   });
 });
